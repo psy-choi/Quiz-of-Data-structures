47cbc3b5d95ec57f13da1441d352f88cb17a07da
psy-choi <84831081+psy-choi@users.noreply.github.com>
2021-05-31 09:34:54 +0900

    Create BJ2609.py

---
+++ BJ2609.py
@@ -0,0 +1,11 @@
+# Quiz-of-Data-structures
+import sys
+A, B = map(int, sys.stdin.readline().split())
+#유클리드 호제법
+a, b = A, B
+while b != 0:
+    a %= b
+    a,b = b,a
+
+print(a)
+print(A*B//a) #//로 표현하는 것은 float값으로 하지 않기 위함
